ab791e0268133bf096fd03fd0bebd61455f64952
IDMT-MK <107912869+IDMT-MK@users.noreply.github.com>
2023-12-04 15:22:58 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// JavaScript
+
+const thumbs = document.querySelectorAll('.thumb');
+const main = document.querySelector('#main-image');
+const text = document.querySelector('#caption');
+const loading = document.getElementById('loading');
+
+function animation(){
+  loading.classList.add('loaded');
+}
+window.addEventListener('load', animation);
+
+// クリックで切り替え
+// thumbs.forEach(function(item, index) {
+//   item.onclick = function() {
+//     // console.log(this.dataset.image);
+//     main.src = this.dataset.image;
+//   }
+// });
+
+//マウスオーバー
+// thumbs.forEach(function(item, index) {
+//   item.onmouseover = function() {
+    // main.src = this.dataset.image;
+    // text.textContent = this.title;
+//   }
+// });
+
+//イベントリスナーで書き換え
+// thumbs.forEach(function (item, index) {
+//   function change() {
+//     main.src = this.dataset.image;
+//   }
+//   item.addEventListener('mouseover', change);
+// });
